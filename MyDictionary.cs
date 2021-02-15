6a67e53ffb731ca3da3b847ca72a7188a799fa1f
aleynayilmazz <69020327+aleynayilmazz@users.noreply.github.com>
2021-02-16 00:04:44 +0300

    Initial commit

---
+++ MyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryOlusturma
+{
+    //List<string> sehirler = new List<string>();
+    class MyDictionary<T>
+    {
+        //ürün dizimiz ve elemanları geçici olarak tutacagımız dizi olusturulur.
+        T[] productArray;
+        T[] tempArray;
+
+        public MyDictionary()
+        {
+            //ekleme yapabilmek için, referans tip olduğu için bir array in new lenme zorunluluğu var..
+          
+            productArray = new T[0];//Sıfır elemanlı olarak dizi oluşturulur.
+        }
+
+        public void Add(T product) 
+        {
+            tempArray = productArray;//Elimizdeki ürünleri geçici olarak bir diziye aktarıyoruz.
+            productArray = new T[productArray.Length+1];//eleman sayısı 1 arttı.ama bu şekilde yaparsak referans adresi kayboluyor.
+            //bunun için geçiçi dizi yapılır.
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                productArray[i] = tempArray[i];//geçici diziye aktardığımız ürünleri alıyoruz.
+            }
+            productArray[productArray.Length - 1] = product;//aslında eklenmek istenen urun eklenmiş oldu..
+        }
+
+        public T[] Products
+        {
+            get { return productArray; }
+        }
+
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DictionaryOlusturma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> products = new MyDictionary<string>();//instance oluşturulur.
+            products.Add("Ayakkabı");//ürün eklenir
+            products.Add("Çanta");
+            products.Add("Parfüm");
+            products.Add("Kazak");
+            //eklenen ürünleri listeleme
+            foreach (var product in products.Products)
+            {
+                Console.WriteLine(product);
+            }
+
+            
+        }
+    }
+
+}
